dbab2bd29bde2bfb5303e12682f83563725c99c0
Aurélien Manteaux <amanteaux@coreoz.com>
2023-07-28 11:46:45 +0200

    Start config and interceptor modules

---
+++ router-config/src/main/java/com/coreoz/http/router/config/HttpGatewayRouterConfig.java
@@ -0,0 +1,12 @@
+package com.coreoz.http.router.config;
+
+import com.coreoz.http.router.HttpGatewayRouter;
+import com.typesafe.config.Config;
+
+import javax.inject.Singleton;
+
+public class HttpGatewayRouterConfig {
+    public static <T> HttpGatewayRouter<T> readConfig(Config config) {
+
+    }
+}
